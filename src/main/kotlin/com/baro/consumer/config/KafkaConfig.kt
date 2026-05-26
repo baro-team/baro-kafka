5210909499b30b78708b7965261f1effa3fb26fd
ION127 <ion011227@gmail.com>
2026-05-26 10:23:10 +0900

    feat: Kafka 토픽 자동생성 설정 추가 및 k8s 노드셀렉터 지정

--- src/main/kotlin/com/baro/consumer/config/KafkaConfig.kt
+++ src/main/kotlin/com/baro/consumer/config/KafkaConfig.kt
@@ -3,11 +3,23 @@ package com.baro.consumer.config
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import org.apache.kafka.clients.admin.NewTopic
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.config.TopicBuilder
 
 @Configuration
 class KafkaConfig {
+
+    @Value("\${app.kafka.topic}")
+    private lateinit var topic: String
+
+    @Bean
+    fun vehicleDataTopic(): NewTopic = TopicBuilder.name(topic)
+        .partitions(3)
+        .replicas(2)
+        .build()
 
     @Bean
     fun objectMapper(): ObjectMapper {
